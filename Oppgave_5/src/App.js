d9bdcf97bcd4987c6fb9c99fd97e21353acb1d16
Hawski <69961102+Hawski@users.noreply.github.com>
2021-09-27 11:11:20 +0200

    v.0.0.1
    bare for å ha den på github

---
+++ Oppgave_5/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './styles.scss';
+import MyComponent from './components/MyComponent'
+
+const App = () => {
+    return (
+        <>
+        <h1>The magic happens here</h1>
+        <MyComponent title="It Works"/>
+        </>
+    );
+}
+
+
+
+
+
+export default App;
---
+++ Oppgave_5/src/components/MyComponent.js
@@ -0,0 +1,12 @@
+
+
+const MyComponent = ({title}) => {
+    return(
+    <>
+        <h1>My First Component</h1>
+        <p>{title}</p>
+    </>
+    );
+} 
+
+export default MyComponent;
